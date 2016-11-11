6fe74fc70fe0c0c1374c17220804e9944242b93d
jkrouse13 <jon.krouse@gmail.com>
2016-11-11 09:55:53 -0500

    added bootstrap to the form, still tinkering with the tags to get them to work.

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
-$.getJSON('https://calm-thicket-39625.herokuapp.com/api/notes')
-  .then(function(response){
-    response.notes.forEach(function(note){
-      $('#notes').prepend('<h3>'+ note.title + '</h3>' + '<p>' + note.body + '</p>')
+$(document).ready(function(){
+  $.getJSON('https://calm-thicket-39625.herokuapp.com/api/notes')
+    .then(function(response){
+      response.notes.forEach(function(note){
+        var display_notes = template(note)
+        $('#handlebar_notes').prepend(display_notes)
+      })
     })
+})
+
+
+
+var note_source   = $("#note-template").html()
+var template = Handlebars.compile(note_source)
+
+function handle_note_form(response){
+  var display_notes = template(response.note)
+  $('#handlebar_notes').prepend(display_notes)
+  clear_form('#note')
+}
+function clear_form(selector){
+  $(selector)[0].reset()
+}
+// $.getJSON('https://calm-thicket-39625.herokuapp.com/api/notes')
+//   .then(function(response){
+//     response.notes.forEach(function(note){
+//       var display_notes = template(note)
+//       $('#handlebar_notes').prepend(display_notes)
+//     })
+//   })
+$('#note').on('submit', function (ev){
+  ev.preventDefault()
+  $.post('https://calm-thicket-39625.herokuapp.com/api/notes',
+  $(this).serializeArray()
+).done(function(response){
+  handle_note_form(response)
   })
+})
+$('.tag_link').on('click', function(ev){
+  ev.preventDefault()
+
+})
